904a19e75b0c3b31c9255667d7968e7a03debc22
HumnabadHemanthreddy <humnabadhemanthreddy@gmail.com>
2026-03-12 11:52:44 +0530

    Add files via upload

---
+++ month_dates.js
@@ -0,0 +1,9 @@
+
+let month=7;
+let arr=["janvary","feburary","march","april","may","june","july","august","sep","oct","nov","dec"];
+if(month>=1||month<=12){
+    console.log(arr[month-1]);
+}
+else{
+    console.log("invalid");
+}
