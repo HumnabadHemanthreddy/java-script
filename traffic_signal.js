8d6bef55ba542fe0ebc08ab052f4f0a6574052fd
HumnabadHemanthreddy <humnabadhemanthreddy@gmail.com>
2026-03-12 11:17:29 +0530

    Add files via upload

---
+++ traffic_signal.js
@@ -0,0 +1,14 @@
+let signal = "red";
+switch (true) {
+    case (signal=="green"):
+        console.log("LET GO!")
+        break;
+    case (signal=="yellow"):
+        console.log("READY TO GO !")
+        break;
+    case (signal=="red"):
+        console.log("STOP !")
+        break;
+    default:
+        console.log("invalid")
+}
