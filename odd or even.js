f95718050210178ee8c2b7b4d3a2b5c38d952c7d
HumnabadHemanthreddy <humnabadhemanthreddy@gmail.com>
2026-03-12 10:57:55 +0530

    Add files via upload

---
+++ odd or even.js
@@ -0,0 +1,17 @@
+let n=10;
+if(n%2==0){
+    console.log("even number")
+}
+else{
+    console.log("odd number")
+}
+
+if(n>0){
+    console.log("is a positive number")
+}
+else if(n<0){
+    console.log("is a negative")
+}
+else{
+    console.log("is a zero")
+}
