14ced0c53d22cafe16fb8b1fdfb6a38556340f37
HumnabadHemanthreddy <humnabadhemanthreddy@gmail.com>
2026-03-12 10:58:12 +0530

    Add files via upload

---
+++ student marks.js
@@ -0,0 +1,25 @@
+let n=30;
+if((n>=90)&&(n<80))
+    {
+console.log("A grade")
+}
+else if((n>=79)&&(n<70))
+    {
+console.log("a grade")
+}
+else if((n>=69)&&(n<60))
+    {
+console.log("c grade")
+}
+else if((n>=59)&&(n<50))
+    {
+console.log("a grade")
+}
+else if((n>=49)&&(n<40))
+    {
+console.log("a grade")
+}
+else 
+    {
+console.log("fail")
+}
