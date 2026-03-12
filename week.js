665507804b3bf3aee851eed08a5bb7822b8bd0a4
HumnabadHemanthreddy <humnabadhemanthreddy@gmail.com>
2026-03-12 11:09:18 +0530

    Add files via upload

---
+++ week.js
@@ -0,0 +1,31 @@
+
+let day = 5
+switch (true) {
+    case (day == 1):
+        console.log("monday")
+        break;
+    case (day == 2):
+        console.log("tuesday")
+        break;
+    case (day == 3):
+        console.log("wednesday")
+        break;
+    case (day == 4):
+        console.log("thursday")
+        break;
+    case (day == 5):
+        console.log("friday")
+        break;
+    case (day == 6):
+        console.log("saturday")
+        break;
+    case (day == 7):
+        console.log("sunday")
+        break;
+    default:
+        console.log("the day is invalid")
+
+
+}
+
+
