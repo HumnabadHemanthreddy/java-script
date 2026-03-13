1ae4a3aebea8906adec882c80b936ceeab160e5a
HumnabadHemanthreddy <humnabadhemanthreddy@gmail.com>
2026-03-13 09:57:31 +0500

    Add files via upload

---
+++ celsius_fun.js
@@ -0,0 +1,10 @@
+function checkFreezing(temp) {
+    if (temp <= 0) {
+    console.log("The temperature is at or below freezing point.");
+    } else {
+    console.log("The temperature is above freezing point.");
+    }
+}
+
+checkFreezing(-5);//The temperature is at or below freezing point.celsius_fun.js:3
+checkFreezing(10);//The temperature is above freezing point.
